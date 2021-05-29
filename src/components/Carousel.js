303587f6407ae7fe5b64a5d98561f2c2c8ee8975
AshwinNair-17 <nairashwin17295@gmail.com>
2021-05-29 09:08:43 +0530

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,22 +2,22 @@ let Carousel = ()=>{
 return(
 <div class="d-flex" data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
   <div>
-      <img src= 'fox.jpeg;'/>
+      <img src= {"fox.jpeg"}/>
   </div>
 </div>
 
