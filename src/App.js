3fe291d71989d8d2f163517d4ccd45cd9066c24b
AshwinNair-17 <nairashwin17295@gmail.com>
2021-05-29 08:49:31 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Navbar.js';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Carousel/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Carousel.js
@@ -0,0 +1,27 @@
+let Carousel = ()=>{
+return(
+<div class="d-flex" data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+  <div>
+      <img src= 'fox.jpeg;'/>
+  </div>
+</div>
+
+);
+}
+
+export default Carousel;
